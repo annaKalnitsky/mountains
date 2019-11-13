c8dfb972e13863834084539b15a7a4597adefd5d
annaKalnitsky <53423088+annaKalnitsky@users.noreply.github.com>
2019-11-13 22:16:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+
+//Menu opener hamburger
+
+	$('.menu-open').click(function () {
+		$('.menu-collapse').toggleClass('d-none').css('order','1');
+		$('.menu').toggleClass('menu-opened');
+	});
+
+
+});
+
+
+
+
+
+
+
+
